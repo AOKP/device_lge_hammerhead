44d0e39012cd8cb24ad4baf2686cc9091f00c92d
STELIX <ssspinni@gmail.com>
2015-10-15 19:13:35 +0200

    HammerHead - Disable bootanimation half res...
    ...until we have them.
    
    Change-Id: I838fc663b25135342be37681ce3513cd29fba7e6
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -1,7 +1,7 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
-TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_BOOTANIMATION_HALF_RES := false
 
 # Inherit some common AOKP stuff.
 $(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
