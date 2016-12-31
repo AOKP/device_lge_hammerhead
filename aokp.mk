02bde01dc6158a4fdadc4d8fb853c791f40fa90c
STELIX <ssspinni@gmail.com>
2016-12-31 18:44:59 +0100

    hammerhead - AOKPize
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -3,15 +3,15 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerhead
-PRODUCT_NAME := cm_hammerhead
+PRODUCT_NAME := aokp_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
